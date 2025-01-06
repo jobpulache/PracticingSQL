131bf55fc2766228543a6f17fd1a4a40c595c6cb
Job Pulache <jobpulachecarrenojob837@gmail.com>
2025-01-05 23:59:14 -0500

    more insert into

--- SQLPracticingExerciseBeginners/Exercises.sql
+++ SQLPracticingExerciseBeginners/Exercises.sql
@@ -21,3 +21,17 @@ INSERT INTO Products(product_name, category, unit_price) VALUES ("Laptop Gamer",
 INSERT INTO Products(product_name, category, unit_price) VALUES("Phone", "Electronics", 800.00);
 
 SELECT 	product_name, unit_price FROM PRODUCTS;
+SELECT sale_id, sale_date FROM Sales;
+INSERT INTO Products ( product_name, category, unit_price) VALUES
+('Laptop', 'Electronics', 500.00),
+('Smartphone', 'Electronics', 300.00),
+( 'Headphones', 'Electronics', 30.00),
+( 'Keyboard', 'Electronics', 20.00),
+('Mouse', 'Electronics', 15.00);
+INSERT INTO Sales (product_id, quantity_sold, sale_date, total_price) VALUES
+( 101, 5, '2024-01-01', 2500.00),
+(102, 3, '2024-01-02', 900.00),
+( 103, 2, '2024-01-02', 60.00),
+( 104, 4, '2024-01-03', 80.00),
+( 105, 6, '2024-01-03', 90.00);
+
