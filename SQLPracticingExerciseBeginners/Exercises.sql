e81b44d0bacd29a0a99b52909fe56b73bbf4d7f3
Job Pulache <jobpulachecarrenojob837@gmail.com>
2025-01-05 23:55:11 -0500

    Retrieve the product_name and unit_price from the Products table

--- SQLPracticingExerciseBeginners/Exercises.sql
+++ SQLPracticingExerciseBeginners/Exercises.sql
@@ -14,5 +14,10 @@ unit_prince DECIMAL(10, 2),
 primary KEY(product_id)
 );
 ALTER table Products CHANGE unit_prince unit_price DECIMAL(10, 2);
+ALTER TABLE Sales CHANGE producto_id product_id INT;
 INSERT INTO Sales( producto_id, quantity_sold, sale_date, total_price) VALUES(1, 5, "2025-01-05", 300.00);
+INSERT INTO Sales(product_id, quantity_sold, sale_date, total_price) Values(2, 8, "2025-01-09", 700.00);
 INSERT INTO Products(product_name, category, unit_price) VALUES ("Laptop Gamer", "Electronics", 4000.00);
+INSERT INTO Products(product_name, category, unit_price) VALUES("Phone", "Electronics", 800.00);
+
+SELECT 	product_name, unit_price FROM PRODUCTS;
